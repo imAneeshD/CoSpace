16d5711ac6e46a755885efee08273d2f1ce2ce73
Aneesha D <aneeshad2@gmail.com>
2025-04-27 10:34:35 +0530

    Add topbar component with responsive design; update admin layout and styles

--- src/app/components/pages/admin/admin/admin.component.ts
+++ src/app/components/pages/admin/admin/admin.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SidebarComponent } from "../util/sidebar/sidebar.component";
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { TopbarComponent } from "../util/topbar/topbar.component";
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [SidebarComponent, RouterOutlet, CommonModule],
+  imports: [SidebarComponent, RouterOutlet, CommonModule, TopbarComponent],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss'
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit {
   isMenuOpen: boolean = true;
+  isLargeScreen: boolean = true;
+  
+  constructor() {
+    this.checkScreenSize();
+    window.addEventListener('resize', () => this.checkScreenSize());
+  }
+
+  ngOnInit(): void {
+  }
+
+
+  private checkScreenSize() {
+    this.isLargeScreen = window.innerWidth > 768;
+    this.isMenuOpen = this.isLargeScreen;
+    console.log(this.isMenuOpen);
+  }
 }
